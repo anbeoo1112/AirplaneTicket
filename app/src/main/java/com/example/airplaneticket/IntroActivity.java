c54eeeb13e9853f3a92d912a556a6d70be2b588a
anbeoo1112 <lean1112work1@gmail.com>
2025-04-08 18:25:56 +0700

    update layout

--- app/src/main/java/com/example/airplaneticket/IntroActivity.java
+++ app/src/main/java/com/example/airplaneticket/IntroActivity.java
@@ -11,22 +11,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.airplaneticket.databinding.ActivityIntroBinding;
 
-public class IntroActivity extends AppCompatActivity {
+public class IntroActivity extends BaseActivity {
 private ActivityIntroBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityIntroBinding.inflate(getLayoutInflater());
-        EdgeToEdge.enable(this);
         setContentView(binding.getRoot());
         binding.button.setOnClickListener(v -> {
             // Handle button click
             startActivity(new Intent(this, MainActivity.class));
         });
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
