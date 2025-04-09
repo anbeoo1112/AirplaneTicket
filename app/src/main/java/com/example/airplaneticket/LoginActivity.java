f472110dd3087cc5f1c48a13cbfe7037638f260a
anbeoo1112 <lean1112work1@gmail.com>
2025-04-09 17:10:08 +0700

    add json

---
+++ app/src/main/java/com/example/airplaneticket/LoginActivity.java
@@ -0,0 +1,97 @@
+package com.example.airplaneticket;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.airplaneticket.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GetTokenResult;
+
+public class LoginActivity extends AppCompatActivity {
+    Button btnRegister, btnLogin;
+    EditText edEmail,edPassword;
+    private FirebaseAuth auth;
+    String email,password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+        btnRegister = (Button) findViewById(R.id.btnRegister) ;
+        btnLogin =(Button) findViewById(R.id.btnLogin);
+        edEmail = (EditText)findViewById(R.id.edEmail) ;
+        edPassword = (EditText)findViewById(R.id.edPassword) ;
+        auth = FirebaseAuth.getInstance();
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                email = edEmail.getText().toString();
+                password = edPassword.getText().toString();
+                auth.signInWithEmailAndPassword(email,password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Đăng nhập thành công
+                            FirebaseUser user = auth.getCurrentUser(); // Lấy đối tượng FirebaseUser
+
+                            if (user != null) {
+                                // Lấy ID Token
+                                user.getIdToken(true).addOnCompleteListener(new OnCompleteListener<GetTokenResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<GetTokenResult> task) {
+                                        if (task.isSuccessful()) {
+                                            String idToken = task.getResult().getToken();  // Lấy ID Token
+                                            // Sử dụng ID Token (ví dụ gửi nó lên backend)
+                                            Toast.makeText(getApplicationContext(), "ID Token: " + idToken, Toast.LENGTH_SHORT).show();
+                                            // Chuyển đến MainActivity
+                                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                            startActivity(intent);
+                                        } else {
+                                            Toast.makeText(getApplicationContext(), "Lấy ID Token thất bại", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                });
+                            }
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Đăng nhập thất bại", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+    }
+}
