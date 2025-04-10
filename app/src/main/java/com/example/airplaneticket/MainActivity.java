1fe2c9b34c8da633603d9a3d669319a6c47e97af
anbeoo1112 <lean1112work1@gmail.com>
2025-04-11 00:18:05 +0700

    update main_layout and add SearchActivity backend

--- app/src/main/java/com/example/airplaneticket/MainActivity.java
+++ app/src/main/java/com/example/airplaneticket/MainActivity.java
@@ -1,24 +1,80 @@
 package com.example.airplaneticket;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.airplaneticket.Model.Location;
+import com.example.airplaneticket.databinding.ActivityMainBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class MainActivity extends BaseActivity {
+//    "48:21 cua video roi nhe"
+    private ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        initLocations();
+
+
+    }
+
+    private void initLocations() {
+        binding.progressBarFrom.setVisibility(View.VISIBLE);
+        binding.ProgressBarTo.setVisibility(View.VISIBLE);
+
+        DatabaseReference myRef =database.getReference("Locations");  // Giới hạn số lượng dữ liệu tải về
+
+        ArrayList<Location> list = new ArrayList<>();
+
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    for (DataSnapshot issue : snapshot.getChildren()) {
+                        list.add(issue.getValue(Location.class));
+                    }
+
+                    // Tạo Adapter tùy chỉnh cho Spinner
+                    ArrayAdapter<Location> adapter = new ArrayAdapter<>(MainActivity.this, R.layout.sp_item, list);
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    binding.fromSp.setAdapter(adapter);
+                    binding.toSp.setAdapter(adapter);
+                    binding.fromSp.setSelection(1);
+
+                    // Ẩn ProgressBar sau khi tải xong
+                    binding.progressBarFrom.setVisibility(View.GONE);
+                    binding.ProgressBarTo.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+
+
         });
     }
+
 }
